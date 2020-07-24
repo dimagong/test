2a468d277cefd68d851c38e1b1f30cc1f6c308d0
dimagong <dimagong@gmail.com>
2020-07-25 01:51:06 +0300

    Updates

---
+++ precache-manifest.029466dfb901a33236f5da221a04e2ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7d33b28cea52f64c1e954e1cbed20d",
+    "url": "/test1/index.html"
+  },
+  {
+    "revision": "486b239c7719538e25bd",
+    "url": "/test1/static/css/main.f75bb49c.chunk.css"
+  },
+  {
+    "revision": "3fc2a2671625eb45b49e",
+    "url": "/test1/static/js/2.d42b7930.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test1/static/js/2.d42b7930.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "486b239c7719538e25bd",
+    "url": "/test1/static/js/main.d3b45851.chunk.js"
+  },
+  {
+    "revision": "01b7d770c61490c4bff7",
+    "url": "/test1/static/js/runtime-main.09c877f9.js"
+  }
+]);
